4622c9102c197b45e94861fc00d8fe17b914d24c
Patrick Alexander <patrick_alexander@mail.com>
2019-09-06 05:38:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(a)
+  new = []
+  i = 0
+  while i < a.length
+    new.push(yield(a[i]))
+    i += 1
+  end
+  new
+end
+
+def reduce(a, starting_p=nil)
+  if sp
+    sum = starting_p
+    i = 0
+  else
+    sum = s[0]
+    i = 1
+  end
+  while i < a.length
+    sum = yield(sum, a[i])
+    i += 1
+  end
+  sum
+end
